cb36f4880df9a4bd3dc513d2e277537b2f497174
Mike Denisov <denisov.ua@gmail.com>
2015-12-12 20:22:47 +0200

    initital commit

---
+++ Assets/Scripts/Game Scripts/Behaviours/VideoTexturePlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoTexturePlayer : MonoBehaviour
+{
+
+    [SerializeField] private Renderer _renderer;
+
+    void Start()
+    {
+        var movieTexture = _renderer.material.mainTexture as MovieTexture;
+        movieTexture.Play();        
+    }
+}
